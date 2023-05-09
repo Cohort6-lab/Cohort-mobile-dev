00a3ebf7c13b7627223a7505c21ed9dbd997601b
EricMuoka <ericmuoka1@gmail.com>
2023-05-09 18:35:19 +0100

    initial

---
+++ cohort/App.tsx
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+const App =()=> {
+  return(
+    <SafeAreaView>
+      <Text>cohort mobile app ready to take off</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
